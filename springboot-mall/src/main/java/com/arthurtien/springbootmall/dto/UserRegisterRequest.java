37445ac0b0095796d62fee319e47669d1250225e
Arthur Tien <arhturtien88@gmail.com>
2023-03-06 22:24:05 +0800

    「帳號功能 - 檢查註冊的email, 隱藏返回的密碼」

--- springboot-mall/src/main/java/com/arthurtien/springbootmall/dto/UserRegisterRequest.java
+++ springboot-mall/src/main/java/com/arthurtien/springbootmall/dto/UserRegisterRequest.java
@@ -1,5 +1,6 @@
 package com.arthurtien.springbootmall.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
@@ -7,6 +8,7 @@ import lombok.Data;
 public class UserRegisterRequest {
 
     @NotBlank
+    @Email
     private String email;
 
     @NotBlank
--- springboot-mall/src/main/java/com/arthurtien/springbootmall/model/User.java
+++ springboot-mall/src/main/java/com/arthurtien/springbootmall/model/User.java
@@ -1,5 +1,7 @@
 package com.arthurtien.springbootmall.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.util.Date;
@@ -8,8 +10,13 @@ import java.util.Date;
 public class User {
 
     private Integer userId;
+
+//    @JsonProperty("e_mail") // 回傳前端的json 的 email key 變成 e_mail
     private String email;
+
+    @JsonIgnore
     private String password;
+
     private Date createDate;
     private Date lastModifiedDate;
 }
